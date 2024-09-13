f2d40f65ca6f387435c69f66f51c0026c42ca319
Thomas Schmid <thomas@fount.ch>
2024-09-13 22:45:40 +0200

    Update utils.ts

--- src/routes/api/utils.ts
+++ src/routes/api/utils.ts
@@ -11,6 +11,7 @@ export function withCORS(responseInit?: ResponseInit): ResponseInit {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Content-Security-Policy': "frame-ancestors 'self' https://plugins-cdn.datocms.com",
     },
   };
 }
